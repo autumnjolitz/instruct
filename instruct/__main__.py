fefe94d500f28e1ddd3249ca75c176b6a37e49b6
Autumn Jolitz <autumn.jolitz@gmail.com>
2017-10-07 23:26:13 -0700

    feat: Optimize through use of ``__new__`` to seed vital fields ahead of time

--- instruct/__main__.py
+++ instruct/__main__.py
@@ -1,15 +1,62 @@
+from typing import Union
+from instruct import Base
 import timeit
 
 test_statement = '''
 t.name_or_id = 1
 '''
 
+
+class Test(Base):
+    __slots__ = {
+        'name_or_id': Union[int, str]
+    }
+
+    def __init__(self, **kwargs):
+        self.name_or_id = 1
+        super().__init__(**kwargs)
+
+
+class TestOptimized(Base, fast=True):
+    __slots__ = {
+        'name_or_id': Union[int, str]
+    }
+
+    def __init__(self, **kwargs):
+        self.name_or_id = 1
+        super().__init__(**kwargs)
+
+
 def main():
+    ttl = timeit.timeit('t = Test(name_or_id="autumn")', setup='from __main__ import Test')
+    per_round_ms = (ttl / timeit.default_number) * 1000000
+    print('Overhead of allocation, one field, safeties on: {:.2f}ms'.format(per_round_ms))
 
-    print(timeit.timeit(test_statement, setup='from instruct import Test;t = Test()'))
-    print(timeit.timeit(test_statement, setup='from instruct import TestOptimized as Test;t = Test()'))
+    ttl = timeit.timeit('t = Test(name_or_id="autumn")', setup='from __main__ import TestOptimized as Test')
+    per_round_ms = (ttl / timeit.default_number) * 1000000
+    print('Overhead of allocation, one field, safeties off: {:.2f}ms'.format(per_round_ms))
 
-    print("This function is easily called using 'python -m module_template'!")
+    print('Overhead of setting a field:')
+    ttl = timeit.timeit(test_statement, setup='from __main__ import Test;t = Test()')
+    per_round_ms = (ttl / timeit.default_number) * 1000000
+    print('Test with safeties: {:.2f} us'.format(per_round_ms))
+
+    ttl = timeit.timeit(test_statement, setup='from __main__ import TestOptimized as Test;t = Test()')
+    per_round_ms = (ttl / timeit.default_number) * 1000000
+    print('Test without safeties: {:.2f} us'.format(per_round_ms))
+
+    print('Overhead of clearing/setting')
+    ttl = timeit.timeit(
+        't.clear();t.name_or_id = 1', setup='from __main__ import Test;t = Test(name_or_id="autumn")')
+    per_round_ms = (ttl / timeit.default_number) * 1000000
+    print('Test with safeties: {:.2f} us'.format(per_round_ms))
+
+    ttl = timeit.timeit(
+        't.clear();t.name_or_id = 1', setup='from __main__ import TestOptimized as Test;t = Test(name_or_id="autumn")')
+    per_round_ms = (ttl / timeit.default_number) * 1000000
+    print('Test without safeties: {:.2f} us'.format(per_round_ms))
+
+
 
 if __name__ == '__main__':
     main()
