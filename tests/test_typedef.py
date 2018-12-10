58feb634e36746796f16fc18d6ae9281ae350830
Autumn Jolitz <autumn.jolitz@gmail.com>
2018-12-10 14:32:08 -0800

    [*] updates for 3.7

---
+++ tests/test_typedef.py
@@ -0,0 +1,10 @@
+from instruct import parse_typedef
+from typing import List, Union, AnyStr, Any
+
+
+def test_parse_typedef():
+    assert parse_typedef(Union[int, AnyStr]) == (int, str, bytes,)
+    assert parse_typedef(List[int]) == (list,)
+    assert parse_typedef(List[Union[int, str]]) == (list,)
+    assert parse_typedef(List[Union[int, AnyStr]]) == (list,)
+    assert parse_typedef(Union[Any, AnyStr]) == (object, str, bytes,)
