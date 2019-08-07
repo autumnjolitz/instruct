12696b4961dcf40ba3fa4335c4dcf9f7e54d3d0b
Autumn Jolitz <autumn.jolitz@gmail.com>
2019-08-07 15:26:16 -0700

    [0.3.5] explicitly support Tuple[type, ...], Dict[key_type, value_type]

--- tests/test_typedef.py
+++ tests/test_typedef.py
@@ -1,7 +1,7 @@
 import pytest
 from enum import Enum
 from instruct.typedef import parse_typedef, make_custom_typecheck
-from typing import List, Union, AnyStr, Any, Optional, Generic, TypeVar, Tuple
+from typing import List, Union, AnyStr, Any, Optional, Generic, TypeVar, Tuple, FrozenSet, Set, Dict
 
 NoneType = type(None)
 
@@ -10,58 +10,71 @@ def test_parse_typedef():
 
     custom_type = make_custom_typecheck(lambda val: val == 3)
     assert isinstance(3, custom_type)
-    assert not isinstance('a', custom_type)
+    assert not isinstance("a", custom_type)
 
-    assert parse_typedef(Union[int, AnyStr]) == (int, str, bytes,)
+    assert parse_typedef(Union[int, AnyStr]) == (int, str, bytes)
 
     ListOfInts = parse_typedef(List[int])
     assert isinstance([1, 2, 3], ListOfInts)
     assert not isinstance([1, 2, 3.1], ListOfInts)
     ListOfIntsOrStrs = parse_typedef(List[Union[int, str]])
-    assert isinstance(['a', 1, 2], ListOfIntsOrStrs)
-    assert not isinstance(['a', 1, 2, {}], ListOfIntsOrStrs)
+    assert isinstance(["a", 1, 2], ListOfIntsOrStrs)
+    assert not isinstance(["a", 1, 2, {}], ListOfIntsOrStrs)
     ListOfIntsOrAnyStr = parse_typedef(List[Union[int, AnyStr]])
-    assert isinstance(['a', b'b', 1], ListOfIntsOrAnyStr)
-    assert not isinstance(['a', b'b', 1, [1]], ListOfIntsOrAnyStr)
+    assert isinstance(["a", b"b", 1], ListOfIntsOrAnyStr)
+    assert not isinstance(["a", b"b", 1, [1]], ListOfIntsOrAnyStr)
     Anything = parse_typedef(Union[Any, AnyStr])
     assert isinstance({}, Anything)
-    assert isinstance(b'a', Anything)
+    assert isinstance(b"a", Anything)
     assert isinstance(type, Anything)
     assert parse_typedef(Union[str, int, float]) == (str, int, float)
     assert parse_typedef(Optional[str]) == (str, NoneType)
-    assert isinstance([['a', 1]], parse_typedef(List[List[Union[int, str]]]))
-    assert isinstance([('a', 1), ('b', 2)], parse_typedef(List[Tuple[int, str]]))
-    assert not isinstance([['a', 1]], parse_typedef(List[Tuple[int, str]]))
+    assert isinstance([["a", 1]], parse_typedef(List[List[Union[int, str]]]))
+    assert isinstance([("a", 1), ("b", 2)], parse_typedef(List[Tuple[str, int]]))
+    assert not isinstance([["a", 1]], parse_typedef(List[Tuple[int, str]]))
     new_type = parse_typedef((List[int], List[List[dict]]))
     assert isinstance([1, 2, 3], new_type)
     assert isinstance([[{}]], new_type)
     assert not isinstance([1.2], new_type)
     type_a = parse_typedef(Union[List[Tuple[str, int]], List[int], List[float]])
     type_b = parse_typedef(List[List[Union[str, int, float]]])
-    assert isinstance([('a', 1)], type_a)
-    assert isinstance([['a', 1]], type_b)
+    assert isinstance([("a", 1)], type_a)
+    assert isinstance([["a", 1]], type_b)
+
+    assert not isinstance([["a", "a"]], type_a)
+    assert not isinstance([[1, "a"]], type_a)
+    # homogenous tuple type:
+    assert isinstance(tuple([1, 1, 1, 1]), parse_typedef(Tuple[int, ...]))
+    assert not isinstance(tuple([1, 1, 1, 1]), parse_typedef(Tuple[str, ...]))
+    assert isinstance(tuple(["a", "b", "c"]), parse_typedef(Tuple[str, ...]))
+
+    TypedDict = parse_typedef(Dict[Union[str, int, float], str])
+
+    assert isinstance({1.0: "a", 4: "b", "c": "d"}, TypedDict)
+    assert not isinstance({1.0: "a", 4: "b", "c": "d", object(): "foo"}, TypedDict)
+    assert not isinstance({1.0: "a", 4: "b", "c": object()}, TypedDict)
 
 
 def test_enum():
     class MyEnum(Enum):
-        A = 'a'
-        B = 'b'
+        A = "a"
+        B = "b"
+
     types = parse_typedef(Optional[MyEnum])
     assert isinstance(types, tuple)
     assert all(isinstance(x, type) for x in types)
     assert isinstance(None, types)
     assert isinstance(MyEnum.A, types)
-    assert not isinstance('a', types)
+    assert not isinstance("a", types)
 
 
 def test_custom_name():
     types = parse_typedef(Union[List[str], List[float]])
-    assert not frozenset(x.__name__ for x in types) - \
-        frozenset(['List[str]', 'List[float]'])
+    assert not frozenset(x.__name__ for x in types) - frozenset(["List[str]", "List[float]"])
 
 
 def test_generic():
-    T = TypeVar('T')
+    T = TypeVar("T")
 
     class Item(Generic[T]):
         pass
@@ -70,3 +83,12 @@ def test_generic():
     # To do: support generics?
     with pytest.raises(NotImplementedError):
         parse_typedef(Item[int])
+
+
+def test_set():
+    type_a = parse_typedef(Set[str])
+    type_b = parse_typedef(FrozenSet[str])
+    assert isinstance({"a", "b"}, type_a)
+    assert not isinstance({"a", "b"}, type_b)
+    assert isinstance(frozenset({"a", "b"}), type_b)
+    assert not isinstance(frozenset({"a", "b"}), type_a)
