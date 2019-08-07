8903c5b41f95d126d4cf07b7afdebaa7151fcb93
Autumn Jolitz <autumn.jolitz@gmail.com>
2019-08-07 15:26:16 -0700

    feat: explicitly support ``Tuple[Type, ...]``, ``Dict[KeyType, ValueType]``, bump to v0.3.5

--- instruct/typedef.py
+++ instruct/typedef.py
@@ -1,15 +1,16 @@
 from typing import Type
 import collections.abc
-from typing import Union, Any, AnyStr, List, Tuple, cast
+from collections.abc import Mapping as AbstractMapping
+from typing import Union, Any, AnyStr, List, Tuple, cast, Optional, Callable
 
 from .utils import flatten
 from .typing import ICustomTypeCheck
 
 
 def make_custom_typecheck(func) -> Type[ICustomTypeCheck]:
-    '''Create a custom type that will turn `isinstance(item, klass)` into `func(item)`
-    '''
-    typename = 'WrappedType<{}>'
+    """Create a custom type that will turn `isinstance(item, klass)` into `func(item)`
+    """
+    typename = "WrappedType<{}>"
 
     class WrappedType(type):
         __slots__ = ()
@@ -30,47 +31,44 @@ def make_custom_typecheck(func) -> Type[ICustomTypeCheck]:
             _WrappedType.__name__ = name
             _WrappedType._name__ = name
             return name
+
     return cast(Type[ICustomTypeCheck], _WrappedType)
 
 
 def create_custom_type(container_type, *args):
-    if getattr(container_type, '__module__', None) == 'typing':
-        if hasattr(container_type, '_name') and container_type._name is None \
-                and container_type.__origin__ is Union:
+    if getattr(container_type, "__module__", None) == "typing":
+        if (
+            hasattr(container_type, "_name")
+            and container_type._name is None
+            and container_type.__origin__ is Union
+        ):
             types = flatten(
-                (create_custom_type(arg) for arg in container_type.__args__), eager=True)
+                (create_custom_type(arg) for arg in container_type.__args__), eager=True
+            )
 
             def test_func(value):
                 return isinstance(value, types)
+
         elif container_type is AnyStr:
             return (bytes, str)
         elif container_type is Any:
             return object
-        elif isinstance(getattr(container_type, '__origin__', None), type) and (
-                issubclass(container_type.__origin__, collections.abc.Iterable) and
-                issubclass(container_type.__origin__, collections.abc.Container)):
+        elif isinstance(getattr(container_type, "__origin__", None), type) and (
+            issubclass(container_type.__origin__, collections.abc.Iterable)
+            and issubclass(container_type.__origin__, collections.abc.Container)
+        ):
             return parse_typedef(container_type)
         else:
             raise NotImplementedError(container_type, container_type._name)
     elif isinstance(container_type, type) and (
-            issubclass(container_type, collections.abc.Iterable) and
-            issubclass(container_type, collections.abc.Container)):
-        test_types = []
-        for some_type in args:
-            test_types.append(create_custom_type(some_type))
-        test_types = tuple(test_types)
-        if test_types:
-            def test_func(value):
-                if not isinstance(value, container_type):
-                    return False
-                return all(isinstance(item, test_types) for item in value)
-        else:
-            def test_func(value):
-                return isinstance(value, container_type)
+        issubclass(container_type, collections.abc.Iterable)
+        and issubclass(container_type, collections.abc.Container)
+    ):
+        test_func = create_typecheck_container(container_type, args)
     elif isinstance(container_type, type) and not args:
         return container_type
     else:
-        assert isinstance(container_type, tuple)
+        assert isinstance(container_type, tuple), f"container_type is {container_type}"
 
         def test_func(value):
             return isinstance(value, container_type)
@@ -78,10 +76,81 @@ def create_custom_type(container_type, *args):
     return make_custom_typecheck(test_func)
 
 
+def create_typecheck_container(container_type, items: Tuple[Any]):
+    test_types = []
+    test_func: Optional[Callable[[Any], bool]] = None
+
+    if issubclass(container_type, tuple):
+        container_type = tuple
+        # Special support: Tuple[type, ...]
+        if any(item is Ellipsis for item in items):
+            if len(items) != 2:
+                raise TypeError("Tuple[type, ...] is allowed but it must be a two pair tuple!")
+            homogenous_type, ellipsis = items
+            if ellipsis is not Ellipsis or homogenous_type is Ellipsis:
+                raise TypeError(
+                    "Tuple[type, ...] is allowed but it must have ellipsis as second arg"
+                )
+
+            def test_func(value):
+                if not isinstance(value, container_type):
+                    return False
+                return all(isinstance(item, homogenous_type) for item in value)
+
+            return test_func
+
+        else:
+            for some_type in items:
+                test_types.append(create_custom_type(some_type))
+
+            def test_func(value):
+                if not isinstance(value, container_type):
+                    return False
+                if len(value) != len(test_types):
+                    raise ValueError(f"Expecting a {len(test_types)} value tuple!")
+                for index, (item, item_type) in enumerate(zip(value, test_types)):
+                    if not isinstance(item, item_type):
+                        # raise TypeError(f"{item!r} at index {index} should be a {item_type}")
+                        return False
+                return True
+
+    elif issubclass(container_type, AbstractMapping):
+        if items:
+            key_type_spec, value_type_spec = items
+            key_type = parse_typedef(key_type_spec)
+            value_type = parse_typedef(value_type_spec)
+
+            def test_func(mapping) -> bool:
+                if not isinstance(mapping, container_type):
+                    return False
+                for key, value in mapping.items():
+                    if not all((isinstance(key, key_type), isinstance(value, value_type))):
+                        return False
+                return True
+
+    if test_func is None:
+        if items:
+            for some_type in items:
+                test_types.append(create_custom_type(some_type))
+            test_types = tuple(test_types)
+
+            def test_func(value):
+                if not isinstance(value, container_type):
+                    return False
+                return all(isinstance(item, test_types) for item in value)
+
+        else:
+
+            def test_func(value):
+                return isinstance(value, container_type)
+
+    return test_func
+
+
 def is_typing_definition(item):
     if not isinstance(item, type):
         return is_typing_definition(type(item))
-    if getattr(item, '__module__', None) == 'typing':
+    if getattr(item, "__module__", None) == "typing":
         return True
     return False
 
@@ -93,25 +162,27 @@ def parse_typedef(typedef: Union[Tuple[Type, ...], List[Type]]) -> Union[Type, T
         return typedef
 
     if typedef is AnyStr:
-        return str, bytes,
+        return str, bytes
     elif typedef is Any:
         return object
     elif typedef is Union:
-        raise TypeError('A bare union means nothing!')
-    elif hasattr(typedef, '_name'):
+        raise TypeError("A bare union means nothing!")
+    elif hasattr(typedef, "_name"):
         if typedef._name is None:
             # special cases!
             if typedef.__origin__ is Union:
                 return flatten(
-                    (parse_typedef(argument) for argument in typedef.__args__),
-                    eager=True)
+                    (parse_typedef(argument) for argument in typedef.__args__), eager=True
+                )
             raise NotImplementedError(
-                f'The type definition for {typedef} is not supported, report as an issue.')
-        if hasattr(typedef, '_special'):
+                f"The type definition for {typedef} is not supported, report as an issue."
+            )
+        if hasattr(typedef, "_special"):
             if not typedef._special:  # this typedef is specific!
                 cls = create_custom_type(typedef.__origin__, *typedef.__args__)
-                cls.set_name(str(typedef).replace('typing.', ''))
+                cls.set_name(str(typedef).replace("typing.", ""))
                 return cls
-        return typedef.__origin__,
+        return (typedef.__origin__,)
     raise NotImplementedError(
-        f'The type definition for {typedef} is not supported, report as an issue.')
+        f"The type definition for {typedef} is not supported, report as an issue."
+    )
--- tests/test_atomic.py
+++ tests/test_atomic.py
@@ -11,10 +11,7 @@ from instruct import Base, add_event_listener, ClassCreationFailed
 
 
 class Data(Base, history=True):
-    __slots__ = {
-        'field': Union[str, int],
-        'other': str,
-    }
+    __slots__ = {"field": Union[str, int], "other": str}
 
     def __init__(self, **kwargs):
         self.field = 0
@@ -22,62 +19,47 @@ class Data(Base, history=True):
 
 
 class NestedData(Base):
-    '''
+    """
     And embedded dict should mean a class will be spawned and stored at the value
     location
-    '''
-    __slots__ = {
-        'id': int,
-        'nested': {
-            'field': str,
-        }
-    }
+    """
+
+    __slots__ = {"id": int, "nested": {"field": str}}
 
 
 class Field(Base):
-    __slots__ = {
-        'field': str
-    }
+    __slots__ = {"field": str}
 
 
 class Child(Field):
-    __slots__ = {
-        'another': str
-    }
+    __slots__ = {"another": str}
 
 
 class NestedDataAlt(Base):
-    __slots__ = {
-        'id': int,
-        'nested': Field,
-    }
+    __slots__ = {"id": int, "nested": Field}
 
 
 class LinkedFields(Base):
-    __slots__ = {
-        'id': int,
-        'name': str,
-    }
+    __slots__ = {"id": int, "name": str}
 
-    __coerce__ = {
-        'id': (str, lambda obj: int(obj, 10))
-    }
+    __coerce__ = {"id": (str, lambda obj: int(obj, 10))}
 
     def __init__(self, **kwargs):
         self.id = 0
         super().__init__(**kwargs)
 
-    @add_event_listener('id')
+    @add_event_listener("id")
     def _on_id_change(self, old, new):
         if new == -1:
-            self.name = 'invalid'
+            self.name = "invalid"
 
 
 def test_inheritance():
-    child = Child(field='a', another='b')
-    assert child.field == 'a'
-    assert child.another == 'b'
-    assert child._column_types['field'] is str
+    child = Child(field="a", another="b")
+    assert child.field == "a"
+    assert child.another == "b"
+    assert child._column_types["field"] is str
+
 
 def test_mapping():
     l1 = LinkedFields(id=2, name='Autumn')
@@ -111,36 +93,32 @@ def test_json():
 
 def test_json_complex():
     class SubItems(Base):
-        __slots__ = {
-            'value': int
-        }
+        __slots__ = {"value": int}
 
     class Item(Base):
-        __slots__ = {
-            'collection': List[SubItems]
-        }
+        __slots__ = {"collection": List[SubItems]}
         __coerce__ = {
-            'collection': (List[dict], lambda items: [SubItems(**item) for item in items])
+            "collection": (List[dict], lambda items: [SubItems(**item) for item in items])
         }
 
-    a = Item(collection=[{'value': 1}, {'value': -1}])
+    a = Item(collection=[{"value": 1}, {"value": -1}])
     a_json = a.to_json()
-    assert isinstance(a_json['collection'][0], dict)
-    assert isinstance(a_json['collection'][1], dict)
-    assert isinstance(a['collection'][0], SubItems)
+    assert isinstance(a_json["collection"][0], dict)
+    assert isinstance(a_json["collection"][1], dict)
+    assert isinstance(a["collection"][0], SubItems)
 
 
 def test_coercion():
     l = LinkedFields(id='2', name='Autumn')
     assert l.id == 2
-    l.id = '5'
+    l.id = "5"
     assert l.id == 5
 
 
 def test_event_listener():
     l = LinkedFields(id=2, name='Autumn')
     l.id = -1
-    assert l.name == 'invalid'
+    assert l.name == "invalid"
 
 
 def test_derived_klass():
@@ -159,10 +137,10 @@ def test_derived_equivalence():
 
 def test_valid_types():
     t = Data(field=1)
-    assert '__dict__' not in dir(t)
+    assert "__dict__" not in dir(t)
     assert t.field == 1
-    t.field = 't'
-    assert t.field == 't'
+    t.field = "t"
+    assert t.field == "t"
 
 
 def test_invalid_types():
@@ -175,21 +153,29 @@ def test_history():
     t.field = 'not autumn'
     for change in t.list_changes():
         print(change)
-        print('[{0.timestamp}] [{0.delta.state}] {0.key} -> {0.delta.old} -> {0.delta.new}'.format(change))
+        print(
+            "[{0.timestamp}] [{0.delta.state}] {0.key} -> {0.delta.old} -> {0.delta.new}".format(
+                change
+            )
+        )
 
 
 def test_history_autoprune():
-    '''
+    """
     A historical object should be smart enough to recognize when it's not really changed
-    '''
-    t = Data(field='autumn')
-    t.field = 'not autumn'
-    t.field = 'autumn'
+    """
+    t = Data(field="autumn")
+    t.field = "not autumn"
+    t.field = "autumn"
     changes = tuple(t.list_changes())
     for change in changes:
-        print('[{0.timestamp}] [{0.delta.state}] {0.key} -> {0.delta.old} -> {0.delta.new}'.format(change))
+        print(
+            "[{0.timestamp}] [{0.delta.state}] {0.key} -> {0.delta.old} -> {0.delta.new}".format(
+                change
+            )
+        )
     assert len(changes) == 3
-    assert not t.is_dirty, 'object was cleanly initialized and not effectively changed'
+    assert not t.is_dirty, "object was cleanly initialized and not effectively changed"
 
 
 def test_changed():
@@ -197,7 +183,7 @@ def test_changed():
     t.field = 'Autumn'
     assert t.is_dirty, 'Item should be dirty - we added a property!'
     t.field = 0
-    assert not t.is_dirty, 'Item is now reset'
+    assert not t.is_dirty, "Item is now reset"
 
 
 def test_reset():
@@ -209,69 +195,58 @@ def test_reset():
 
 def test_fast_setter():
     class FastData(Base, fast=True):
-        __slots__ = {
-            'a': int,
-            'bar': str
-        }
+        __slots__ = {"a": int, "bar": str}
 
-        @add_event_listener('a')
+        @add_event_listener("a")
         def on_a_set(self, old, new):
             self.bar = str(new)
 
     f = FastData(a=1)
-    assert f.bar == '1'
+    assert f.bar == "1"
 
 
 def test_getitem():
     class Foo(Base):
-        __slots__ = {
-            'a': int,
-            'b': str
-        }
+        __slots__ = {"a": int, "b": str}
 
         def some_func():
             return 1
 
-    f = Foo(a=1, b='s')
-    assert f['a'] == 1
-    assert f['b'] == 's'
+    f = Foo(a=1, b="s")
+    assert f["a"] == 1
+    assert f["b"] == "s"
 
-    assert {**f} == {'a': 1, 'b': 's'}
-    f['b'] = 'New str'
-    assert f['b'] == 'New str'
+    assert {**f} == {"a": 1, "b": "s"}
+    f["b"] = "New str"
+    assert f["b"] == "New str"
     with pytest.raises(TypeError):
-        f['b'] = 1234
+        f["b"] = 1234
 
 
 def test_readme():
-
     class Member(Base):
-        __slots__ = {
-            'first_name': str,
-            'last_name': str,
-            'id': int,
-        }
+        __slots__ = {"first_name": str, "last_name": str, "id": int}
 
         def __init__(self, **kwargs):
-            self.first_name = self.last_name = ''
+            self.first_name = self.last_name = ""
             self.id = -1
             super().__init__(**kwargs)
 
     class Organization(Base, history=True):
         __slots__ = {
-            'name': str,
-            'id': int,
-            'members': List[Member],
-            'created_date': datetime.datetime,
+            "name": str,
+            "id": int,
+            "members": List[Member],
+            "created_date": datetime.datetime,
         }
 
         __coerce__ = {
-            'created_date': (str, lambda obj: datetime.datetime.strptime('%Y-%m-%d', obj)),
-            'members': (list, lambda val: [Member(**item) for item in val])
+            "created_date": (str, lambda obj: datetime.datetime.strptime("%Y-%m-%d", obj)),
+            "members": (list, lambda val: [Member(**item) for item in val]),
         }
 
         def __init__(self, **kwargs):
-            self.name = ''
+            self.name = ""
             self.id = -1
             self.members = []
             self.created_date = datetime.datetime.utcnow()
@@ -280,16 +255,10 @@ def test_readme():
     data = {
         "name": "An Org",
         "id": 123,
-        "members": [
-            {
-                "id": 551,
-                "first_name": "Jinja",
-                "last_name": "Ninja",
-            }
-        ]
+        "members": [{"id": 551, "first_name": "Jinja", "last_name": "Ninja"}],
     }
     org = Organization(**data)
-    assert org.members[0].first_name == 'Jinja'
+    assert org.members[0].first_name == "Jinja"
     org.name = "New Name"
     print(tuple(org.list_changes()))
     types = frozenset((type(x) for _, x in org))
@@ -299,27 +268,18 @@ def test_readme():
 
 
 def test_invalid_kwargs():
-
     class Test(Base):
-        __slots__ = {
-            'foo': str,
-            'bar': float,
-            'blech': int,
-        }
+        __slots__ = {"foo": str, "bar": float, "blech": int}
 
     with pytest.raises(ClassCreationFailed) as e:
-        Test(foo=1.2, bar=1, blech='2')
+        Test(foo=1.2, bar=1, blech="2")
     assert len(e.value.errors) == 3
     print(e.value.to_json())
 
 
 def test_properties():
     class Test(Base):
-        __slots__ = {
-            'foo': str,
-            'bar': float,
-            'blech': int,
-        }
+        __slots__ = {"foo": str, "bar": float, "blech": int}
 
         @property
         def yoo(self):
@@ -328,63 +288,54 @@ def test_properties():
         @yoo.setter
         def yoo(self, val):
             self.foo = val
-    assert Test._properties | Test._columns.keys() == frozenset(['foo', 'bar', 'blech', 'yoo'])
 
-    t = Test(yoo='New foo')
-    assert t.foo == 'New foo'
+    assert Test._properties | Test._columns.keys() == frozenset(["foo", "bar", "blech", "yoo"])
+
+    t = Test(yoo="New foo")
+    assert t.foo == "New foo"
 
 
 def test_coerce_complex():
     class ItemItem(Base):
-        __slots__ = {
-            'value': int
-        }
+        __slots__ = {"value": int}
 
     class Item(Base):
-        __slots__ = {
-            'value': List[ItemItem]
-        }
+        __slots__ = {"value": List[ItemItem]}
 
-        __coerce__ = {
-            'value': (List[dict], lambda items: [ItemItem(**item) for item in items])
-        }
+        __coerce__ = {"value": (List[dict], lambda items: [ItemItem(**item) for item in items])}
 
-    f = Item(value=[{'value': 1}, {'value': 2}])
+    f = Item(value=[{"value": 1}, {"value": 2}])
     assert tuple(item.value for item in f.value) == (1, 2)
-    with pytest.raises(TypeError, message='^Unable to set value'):
+    with pytest.raises(TypeError, match="Unable to set value"):
         Item(value=[1, 2])
 
     class SomeEnum(Enum):
-        VALUE = 'value'
+        VALUE = "value"
 
     def parse(items):
         return [tuple(item) for item in items]
 
     class ComplextItem(Base):
         __slots__ = {
-            'name': str,
-            'type': SomeEnum,
-            'value': Union[List[Tuple[str, int]], List[int], List[float]],
+            "name": str,
+            "type": SomeEnum,
+            "value": Union[List[Tuple[str, int]], List[int], List[float]],
         }
 
         __coerce__ = {
-            'type': (str, lambda val: SomeEnum(val)),
-            'value': (List[List[Union[str, int, float]]], parse)
+            "type": (str, lambda val: SomeEnum(val)),
+            "value": (List[List[Union[str, int, float]]], parse),
         }
 
     class VectoredItems(Base):
-        __slots__ = {
-            'items': List[ComplextItem],
-        }
+        __slots__ = {"items": List[ComplextItem]}
 
-        __coerce__ = {
-            'items': (List[dict], lambda items: [ComplextItem(**item) for item in items])
-        }
+        __coerce__ = {"items": (List[dict], lambda items: [ComplextItem(**item) for item in items])}
 
-    c = ComplextItem(value=[['a', 1], ['b', 2]], name='ab', type='value')
-    assert c.value == [('a', 1), ('b', 2)]
+    c = ComplextItem(value=[["a", 1], ["b", 2]], name="ab", type="value")
+    assert c.value == [("a", 1), ("b", 2)]
 
-    VectoredItems(items=[{'value': [['a', 1], ['b', 2]], 'name': 'ab', 'type': 'value'}])
+    VectoredItems(items=[{"value": [["a", 1], ["b", 2]], "name": "ab", "type": "value"}])
 
 
 def test_qulaname():
@@ -392,50 +343,44 @@ def test_qulaname():
         return [tuple(item) for item in items]
 
     class SomeEnum(Enum):
-        VALUE = 'value'
+        VALUE = "value"
 
     class ComplextItem(Base):
         __slots__ = {
-            'name': str,
-            'type': SomeEnum,
-            'value': Union[List[Tuple[str, int]], List[int], List[float]],
+            "name": str,
+            "type": SomeEnum,
+            "value": Union[List[Tuple[str, int]], List[int], List[float]],
         }
 
         __coerce__ = {
-            'type': (str, lambda val: SomeEnum(val)),
-            'value': (List[List[Union[str, int, float]]], parse)
+            "type": (str, lambda val: SomeEnum(val)),
+            "value": (List[List[Union[str, int, float]]], parse),
         }
 
     class VectoredItems(Base):
-        __slots__ = {
-            'items': List[ComplextItem],
-        }
+        __slots__ = {"items": List[ComplextItem]}
 
-        __coerce__ = {
-            'items': (List[dict], lambda items: [ComplextItem(**item) for item in items])
-        }
-    assert VectoredItems.__qualname__ == 'test_qulaname.<locals>.VectoredItems'
-    assert VectoredItems._data_class.__qualname__ == 'test_qulaname.<locals>.VectoredItems._VectoredItems'
-    assert VectoredItems.__name__ == 'VectoredItems'
-    assert VectoredItems._data_class.__name__ == '_VectoredItems'
+        __coerce__ = {"items": (List[dict], lambda items: [ComplextItem(**item) for item in items])}
+
+    assert VectoredItems.__qualname__ == "test_qulaname.<locals>.VectoredItems"
+    assert (
+        VectoredItems._data_class.__qualname__
+        == "test_qulaname.<locals>.VectoredItems._VectoredItems"
+    )
+    assert VectoredItems.__name__ == "VectoredItems"
+    assert VectoredItems._data_class.__name__ == "_VectoredItems"
     assert VectoredItems.__module__ is VectoredItems._data_class.__module__
-    assert getattr(VectoredItems, '_VectoredItems') is VectoredItems._data_class
+    assert getattr(VectoredItems, "_VectoredItems") is VectoredItems._data_class
 
 
 def test_redefine_fields():
     class LooseyGooseyItem(Base):
-        __slots__ = {
-            'a': Optional[str],
-            'b': Optional[str],
-            'c': Optional[str],
-        }
+        __slots__ = {"a": Optional[str], "b": Optional[str], "c": Optional[str]}
 
     class ARequired(LooseyGooseyItem):
-        __slots__ = {
-            'a': str,
-        }
+        __slots__ = {"a": str}
 
-    item = ARequired(a='a')
+    item = ARequired(a="a")
     assert item.b is None
     assert item.c is None
     with pytest.raises(ValueError):
