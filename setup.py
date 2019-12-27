fe4d0e2027040ef20f1a07e152d7cb22a2dd41cb
Autumn Jolitz <autumn.jolitz@gmail.com>
2019-12-27 13:48:23 -0800

    Update author information

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if __version__ is None:
     raise IOError('about.py in project lacks __version__!')
 
 setup(name='instruct', version=__version__,
-      author='Ben Jolitz',
+      author='Autumn Jolitz',
       description='',
       long_description=long_description,
       license='BSD',
@@ -34,7 +34,7 @@ setup(name='instruct', version=__version__,
           'test': ['pytest']
       },
       python_requires='>=3',
-      url="https://github.com/benjolitz/instruct",
+      url="https://github.com/autumnjolitz/instruct",
       classifiers=[
           "Development Status :: 3 - Alpha",
           "Topic :: Utilities",
