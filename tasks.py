872fe6980954fa189dbd2ce62cd0959aeba74123
Autumn <autumn.jolitz@gmail.com>
2024-06-25 19:51:07 -0700

    build: ensure sdists do not depend on source control, remove unused functions

--- tasks.py
+++ tasks.py
@@ -4,19 +4,22 @@ import shutil
 import types
 import base64
 import builtins
+import io
 import sys
-from contextlib import suppress, contextmanager
+import tempfile
+import tarfile
+from contextlib import suppress, contextmanager, closing
 from pathlib import Path
-from typing import Type, Union, Dict, Tuple, Iterable, TypeVar
+from typing import Type, Union, Dict, Tuple, Iterable, TypeVar, List, Optional
 
-try:
+if sys.version_info[:2] >= (3, 8):
     from typing import Literal
-except ImportError:
+else:
     from typing_extensions import Literal
 
 
 from invoke.context import Context
-from tasksupport import task, first, InvertedMapping, trim, truncate
+from tasksupport import task
 
 _ = types.SimpleNamespace()
 this = sys.modules[__name__]
@@ -24,29 +27,14 @@ this = sys.modules[__name__]
 DEFAULT_FORMAT = "lines"
 
 
+T = TypeVar("T")
+U = TypeVar("U")
+
+
 def perror(*args, file=None, **kwargs):
     if file is None:
         file = sys.stderr
     return print(*args, file=file, **kwargs)
-
-
-@contextmanager
-def create_environment(*, copy_os_environ: bool = False, **kwargs: str) -> Dict[str, str]:
-    """
-    Returns some common values for Docker builds
-    """
-    environment = {
-        **(os.environ if copy_os_environ else {}),
-        "NO_COLOR": "1",
-        "COMPOSE_DOCKER_CLI_BUILD": "1",
-        "BUILDX_EXPERIMENTAL": "1",
-        "BUILDX_GIT_LABELS": "full",
-        "BUILDKIT_PROGRESS": "plain",
-        "DOCKER_BUILDKIT": "1",
-        "COMPOSE_PROJECT_NAME": _.project_name(silent=True),
-        **kwargs,
-    }
-    return environment
 
 
 @contextmanager
@@ -62,38 +50,112 @@ def cd(path: Union[str, Path]):
         os.chdir(prior_cwd)
 
 
+class Branch(str):
+    __slots__ = ()
+
+    def __repr__(self):
+        return f'{type(self).__name__}("{self!s}")'
+
+
+class Tag(str):
+    __slots__ = ()
+
+    def __repr__(self):
+        return f'{type(self).__name__}("{self!s}")'
+
+
 @task
-def branch_name(context: Context) -> str:
+def branch_name(context: Context) -> Union[Branch, Tag]:
     with suppress(KeyError):
-        return os.environ["GITHUB_REF_NAME"]
+        ref = os.environ["GITHUB_REF"]
+        if ref.startswith("refs/heads/"):
+            return Branch(ref.removeprefix("refs/heads/"))
+        if ref.startswith("refs/tags/"):
+            return Tag(ref.removeprefix("refs/tags/"))
+        if ref.startswith("refs/pull/"):
+            return Branch(os.environ["GITHUB_HEAD_REF"])
     here = this._.project_root(Path, silent=True)
     if (here / ".git").is_dir():
         with suppress(FileNotFoundError):
-            return context.run(f"git -C {here!s} branch --show-current", hide="both").stdout.strip()
+            result = context.run(f"git -C {here!s} branch --show-current", hide="both")
+            assert result is not None
+            branch = result.stdout.strip()
+            if branch:
+                return Branch(branch)
+            result = context.run(
+                f"git -C {here!s} describe --all --contains --abbrev=4 HEAD", hide="both"
+            )
+            assert result is not None
+            if result.stdout.startswith("tags/"):
+                return Tag(result.stdout.strip().removeprefix("tags/"))
         with open(here / ".git" / "HEAD") as fh:
             for line in fh:
+                line = line.strip()
+                if not line:
+                    continue
                 if line.startswith("ref:"):
                     _, line = (x.strip() for x in line.split(":", 1))
-                if line.startswith("refs/heads/"):
-                    return line.removeprefix("refs/heads/")
+                    if line.startswith("refs/heads/"):
+                        return Branch(line.removeprefix("refs/heads/"))
+                    elif line.startswith("refs/tags/"):
+                        return Tag(line.removeprefix("refs/tags/"))
+                    else:
+                        raise ValueError(f"Unknown ref type: {line}")
+                # okay! detached HEAD!~
+                for tag in sorted(
+                    (here / ".git" / "refs" / "tags").iterdir(),
+                    key=lambda o: o.stat().st_mtime,
+                    reverse=True,
+                ):
+                    if tag.is_file() and tag.read_text().strip():
+                        return Tag(tag.name)
+                all_branch_heads = here / ".git" / "refs" / "heads"
+                for root, dirs, files in (here / ".git" / "refs" / "heads").walk():
+                    for file in files:
+                        if file.read_text().strip() == line:
+                            return Branch(str(file).removeprefix(str(all_branch_heads)))
     raise ValueError("Unable to determine branch name!")
 
 
-T = TypeVar("T")
-
-
-def window(iterable: Iterable[T]) -> Iterable[Tuple[T, T]]:
+def window(
+    iterable: Iterable[T], *, terminus: Optional[U] = None
+) -> Iterable[Tuple[T, Union[T, Optional[U]]]]:
     g = iter(iterable)
     item = next(g)
     for next_item in g:
         yield item, next_item
-        next_item = item
+        item = next_item
+    yield item, terminus
 
 
 @task
-def update_changes(context: Context):
-    python_bin = _.python_path(str)
-    context.run(f"{python_bin} -m git_changelog")
+def update_changes(context: Context, version: str = ""):
+    python_bin = _.python_path(str, silent=True)
+    extra = ""
+    if version:
+        extra = f"-B {version}"
+    result = context.run(f"{python_bin} -m git_changelog {extra}", warn=True)
+    assert result is not None
+    if "already in changelog" in result.stderr:
+        perror("No new changes to create changelog with")
+        return
+    with open(_.project_root(Path, silent=True) / "CHANGES.rst", "r+") as fh:
+        body = fh.read()
+        index = body.index(".. |Changes|")
+        index += body[index:].index("\n") + 1
+        with suppress(ValueError):
+            index += body[index:].index(".. |Changes|")
+            index += body[index:].index("\n") + 1
+        in_between = body[index : index + 2]
+        deleted_newline_count = 0
+        while in_between[-1] == "\n":
+            body = body[:index] + body[index + 1 :]
+            in_between = body[index : index + 2]
+            deleted_newline_count += 1
+        if deleted_newline_count:
+            perror(f"deleted {deleted_newline_count} extra newlines")
+            fh.seek(0)
+            fh.write(body)
 
 
 @task
@@ -103,13 +165,13 @@ def test(context: Context):
 
 
 @task
-def setup_metadata() -> Dict[str, str]:
+def setup_metadata() -> Dict[str, Union[str, Tuple[str, ...]]]:
     with open(_.project_root(Path, silent=True) / "setup.cfg") as fh:
         in_multiline = False
         in_metadata = False
-        values = None
+        values: Optional[Union[str, List[str]]] = None
         key = None
-        mapping = {}
+        mapping: Dict[str, Union[str, Tuple[str, ...]]] = {}
         for line in fh:
             if line.startswith("[metadata]"):
                 in_metadata = True
@@ -119,9 +181,12 @@ def setup_metadata() -> Dict[str, str]:
             if in_metadata:
                 if "=" in line and in_multiline:
                     in_multiline = False
-                    mapping[key] = values
+                    assert key is not None
+                    assert isinstance(values, list)
+                    mapping[key] = tuple(values)
                     values = None
                 if in_multiline:
+                    assert isinstance(values, list)
                     values.append(line.strip())
                     continue
                 key, value = [x.strip() for x in line.split("=", 1)]
@@ -130,7 +195,7 @@ def setup_metadata() -> Dict[str, str]:
                     values = []
                     continue
                 mapping[key] = value.strip()
-        return mapping
+    return mapping
 
 
 @task
@@ -151,31 +216,99 @@ def b64decode(value: str, silent: bool = True) -> str:
     return base64.urlsafe_b64decode(value).decode().strip()
 
 
+def get_topmost_directory(p: Path) -> Path:
+    while p.parent.name:
+        p = p.parent
+    return p
+
+
 @task
-def build(context: Context) -> Tuple[Path, ...]:
-    python_bin = _.python_path(str)
-    context.run(f"{python_bin} -m build")
-    return tuple(Path("dist").iterdir())
+def build(context: Context, validate: bool = False) -> Tuple[Path, ...]:
+    """
+    Create special sdists that do not depend on source control and
+    a bdist from said sdist.
 
+    Because we use ``CURRENT_VERSION`` to generate ``instruct/about.py``,
+    a simple sdist will unfortunately lose the information on which source
+    control version it came from.
 
-# @task
-# def get_tags_from(context: Context, image_name: str) -> Iterable[str]:
-#     """
-#     Given an image url, return the repo tags
-#     """
-#     try:
-#         result = context.run(f"docker inspect {image_name}", hide="both")
-#     except UnexpectedExit as e:
-#         if "Error: No such object:" in e.result.stderr:
-#             context.run(f"docker pull {image_name}", env=compose_environ())
-#             result = context.run(f"docker inspect {image_name}", hide="both")
-#         else:
-#             raise
-#     image = json.loads(result.stdout)
-#     results = []
-#     for match in image:
-#         results.extend(match["RepoTags"])
-#     return results
+    *However* the act of generating an sdist will create the ``instruct/about.py``
+    file which we can then ``exec`` and pull out the public-friendly version.
+
+    We will basically replace the ``attr: ...`` callout in setup.cfg with the
+    actual public version, thus satisfying the need to preserve a source-control'd
+    about.py while being relatively repeatable.
+
+    As a safety, we have a ``validate`` option that will try to install
+    the artifacts into a venv and try to import instruct.
+    """
+    python_bin = _.python_path(str, silent=True)
+    dist = _.project_root(Path, silent=True) / "dist"
+    public_version = None
+    files = []
+    with tempfile.TemporaryDirectory() as tmp:
+        t = Path(tmp)
+        del tmp
+        tempdist = t / "dist"
+        tempdist.mkdir()
+        context.run(f"{python_bin} -m build -s -o {t!s}/sdist")
+        (sdist,) = (Path(t).resolve() / "sdist").iterdir()
+        with tarfile.open(sdist) as source, tarfile.open(
+            f"{tempdist!s}/{sdist.name}", "w:gz"
+        ) as dest:
+            top_most = None
+            for metadata in source:
+                buf = None
+                filename = Path(metadata.name)
+                if top_most is None:
+                    top_most = get_topmost_directory(filename)
+                assert top_most is not None
+                if filename.parent.name == top_most.name and filename.name in (
+                    "generate_version.py",
+                    "CURRENT_VERSION.txt",
+                ):
+                    continue
+                if metadata.isfile():
+                    if (filename.parent.name, filename.name) == ("instruct", "about.py"):
+                        gs = {}
+                        buf = source.extractfile(metadata)
+                        assert buf is not None
+                        exec(buf.read(), gs)
+                        buf.seek(0)
+                        public_version = gs["__version_info__"].public
+                    elif (filename.parent.name, filename.name) == (top_most.name, "setup.cfg"):
+                        buf = io.BytesIO()
+                        fh = source.extractfile(metadata)
+                        assert fh is not None
+                        with closing(fh):
+                            for line in fh:
+                                if line.startswith(b"version ="):
+                                    buf.write(b"version = %s\n" % (public_version.encode(),))
+                                else:
+                                    buf.write(line)
+                        metadata.size = buf.tell()
+                        buf.seek(0)
+                    else:
+                        buf = source.extractfile(metadata)
+                        assert buf is not None
+                if buf is not None:
+                    with closing(buf):
+                        dest.addfile(metadata, buf)
+                else:
+                    dest.addfile(metadata)
+        context.run(f"{python_bin} -m venv {t!s}/venv")
+        context.run(
+            f"{t!s}/venv/bin/python -m pip wheel --no-deps file:///{dest.name!s} -w {t!s}/dist"
+        )
+        if validate:
+            for filename in tempdist.iterdir():
+                with tempfile.TemporaryDirectory() as venv:
+                    context.run(f"{python_bin} -m venv {venv}")
+                    context.run(f"{venv!s}/bin/python -m pip install file:///{filename!s}")
+                    context.run(f"{venv!s}/bin/python -c 'import instruct'")
+        for filename in tempdist.iterdir():
+            files.append(filename.rename(dist / filename.name))
+    return tuple(files)
 
 
 @task
@@ -253,7 +386,7 @@ def setup(
     tests: bool = False,
     devel: bool = False,
     project: bool = True,
-    swap_venv_stage: str = None,
+    swap_venv_stage: Optional[str] = None,
 ) -> Path:
     """
     Create the venv for this project.
